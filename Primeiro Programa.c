45ce2488ef8c42ffdcdee743619ba090fc1607f6
Felipe Salgado <104802376+fesalgz@users.noreply.github.com>
2022-10-20 18:55:39 -0300

    Add files via upload

---
+++ Primeiro Programa.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Primeira atividade que o professor colocou no slide.
+int main()
+{
+	printf("\nCiencia da Computacao\n");
+	printf("Autor: Felipe Salgado\n");
+	printf("RA: n sei\n");
+	printf("Endereco Eletronico: n vou colocar aqui\n");
+	return 0;
+}
